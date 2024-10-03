0791f6f1cfeded825fa0ced7be000cb00f85b669
zhoy <zhongyao9903@gmail.com>
2024-10-03 23:16:20 +0200

    New dataset generation

--- GeoL_net/dataset_gen/RGBD2MaskPC_direction.py
+++ GeoL_net/dataset_gen/RGBD2MaskPC_direction.py
@@ -1,5 +1,7 @@
 """
-transfer the RGBD to a mask pointcloud"""
+transfer the RGBD to a mask pointcloud
+only keep the anchor obj
+and gerenate the mask pointcloud of 8 directions"""
 from GeoL_net.dataset_gen.hdf52png import hdf52png
 from GeoL_net.dataset_gen.RGBD2PC import backproject, visualize_points
 import cv2
@@ -13,9 +15,10 @@ import random
 from tqdm import tqdm
 import time
 import matplotlib.pyplot as plt
+import shutil
+import json
 
-
-def RGBD2MaskPC(depth_path, mask_hd5f_path, output_dir, reprocessing_flag=False, depth_removed_obj = None, mask_hd5f_removed_obj = None):
+def RGBD2MaskPC_direction(depth_path, mask_hd5f_path, output_dir, reprocessing_flag=False, depth_removed_obj = None, mask_hd5f_removed_obj = None):
     """ transfer deph and mask hd5f to point cloud(mask)
     """
 
@@ -26,7 +29,7 @@ def RGBD2MaskPC(depth_path, mask_hd5f_path, output_dir, reprocessing_flag=False,
         print(f"{output_dir}/mask_red.ply already exists")
         return None
     #pcd_mask_preprocessing = pcd_mask_preprocessing
-    pcd_mask_preprocessing = pcd_mask_preprocessing_red_label
+    pcd_mask_preprocessing = pcd_mask_preprocessing_direction_red_label
     # get the mask png (with obj)
     mask_img_path = f"{output_dir}/mask_with_obj.png"
     mask_image = hdf52png(hdf5_path=mask_hd5f_path, output_dir=mask_img_path)
@@ -82,9 +85,6 @@ def RGBD2MaskPC(depth_path, mask_hd5f_path, output_dir, reprocessing_flag=False,
         [0,0.6,0.8]
     ])
     points_scene = (cam_rotation_matrix @ points_scene.T).T
-    #centroid = np.mean(points_scene, axis=0)
-    #print("centroid:", centroid)
-    #points_scene = points_scene - centroid
     colors_scene = color[scene_idx[0], scene_idx[1]]
     pcd_scene = visualize_points(points_scene, colors_scene)
 
@@ -92,28 +92,16 @@ def RGBD2MaskPC(depth_path, mask_hd5f_path, output_dir, reprocessing_flag=False,
     if (depth_removed_obj is not None) and (mask_hd5f_removed_obj is not None):
         pass
         points_no_obj_scene = (cam_rotation_matrix @ points_no_obj_scene.T).T
-        #points_no_obj_scene = points_no_obj_scene - centroid
         colors_no_obj_scene = color_no_obj[scene_no_obj_idx[0], scene_no_obj_idx[1]]
         pcd_no_obj_scene = visualize_points(points_no_obj_scene, colors_no_obj_scene)
-        
-        # save the pcd no obj scene
         pcd_no_obj_scene = remove_ground(pcd_no_obj_scene)
-        #pcd_ori_path = f"{output_dir}/pc_ori.ply"
-        #o3d.io.write_point_cloud(pcd_ori_path, pcd_no_obj_scene)
-
+    
     if reprocessing_flag == True:
         if (depth_removed_obj is not None) and (mask_hd5f_removed_obj is not None):
-            points_scene = pcd_mask_preprocessing(points_scene=pcd_scene, points_no_obj_scene=pcd_no_obj_scene)
+            pcd_mask_preprocessing(points_scene=pcd_scene, points_no_obj_scene=pcd_no_obj_scene, output_dir=output_dir)
         else:
-            points_scene = pcd_mask_preprocessing(points_scene=pcd_scene)
-    
-    # remove the ground floor
-    points_scene = remove_ground(points_scene)
-    
+            pcd_mask_preprocessing(points_scene=pcd_scene)
 
-
-    
-    o3d.io.write_point_cloud(pc_mask_path, points_scene)
 
    
 
@@ -128,24 +116,6 @@ def is_green(color, threshold=0.9):
 
 def is_red(color, threshold=0.9):
     return color[0] > threshold and color[1] < (1 - threshold) and color[2] < (1 - threshold)
-
-def check_collision(green_pcd, other_pcd, threshold=0.001):
-    # 计算绿色点云每个点到最近邻点的距离
-    distances = green_pcd.compute_point_cloud_distance(other_pcd)
-    # 如果最近邻点的最小距离小于阈值，则认为存在碰撞
-    # print("distance:", np.min(distances))
-    return np.min(distances) <= threshold
-
-def check_collisiton_batch(green_pcds, other_pcds, threshold=0.001):
-    """
-    check the collision in batch
-
-    Paras:
-        green_pcds: B * N1* 3
-        other_pcds: B * N2 * 3
-    Return:
-        is_collision: B
-    """
 
 
 def compute_distance_matrix(points_a, points_b):
@@ -226,6 +196,122 @@ def pcd_mask_preprocessing(points_scene, points_no_obj_scene = None):
     # 更新点云颜色
     points_scene.colors = o3d.utility.Vector3dVector(heatmap_colors)
     return points_scene
+
+def pcd_mask_preprocessing_direction_red_label(points_scene, points_no_obj_scene = None, output_dir = None):
+    " generate turbo heatmap label in 8 directions"
+    " generate red label"
+
+    colors = np.asarray(points_no_obj_scene.colors)
+    points = np.asarray(points_no_obj_scene.points)
+
+    # get the blue points
+    blue_mask = np.apply_along_axis(is_blue, 1, colors)
+    blue_points = points[blue_mask]
+    blue_pcd = o3d.geometry.PointCloud()
+    blue_pcd.points = o3d.utility.Vector3dVector(blue_points)
+
+    # get the black points
+    black_mask = np.apply_along_axis(is_black, 1, colors)
+    z_max = blue_pcd.get_axis_aligned_bounding_box().get_max_bound()[2]
+    z_center = blue_pcd.get_axis_aligned_bounding_box().get_center()[2]
+    z_range_mask = (points[:, 2] <= z_max + 10) & (points[:, 2] >= z_center)
+    target_mask = z_range_mask & black_mask
+    colors[target_mask] = [1, 0, 0]  # set the color of these points to red, representing the desktop
+    points_no_obj_scene.colors = o3d.utility.Vector3dVector(colors)
+
+    # get the red points
+    red_mask = np.all(colors == [1, 0, 0], axis=1)
+    red_points = points[red_mask]
+
+    # re-extract
+    black_mask = np.apply_along_axis(is_black, 1, colors)
+    blue_mask = np.apply_along_axis(is_blue, 1, colors)
+    red_mask = np.all(colors == [1, 0, 0], axis=1)
+
+    # initialize the heatmap value to 0
+    heatmap = np.zeros(len(points))
+
+    # determin the target point cloud center. center of the blue point cloud bbox
+    blue_pcd_center = blue_pcd.get_center()
+    offset = random.uniform(100, 200)
+    pcd_mask_8_direction_list = []
+
+    for direction in ["Left", "Right", "Front", "Behind", "Left Front", "Left Behind", "Right Front", "Right Behind"]:
+        target_pos = blue_pcd_center.copy()
+        if direction == "Left":
+            target_pos[0] -= offset
+        elif direction == "Right":
+            target_pos[0] += offset
+        elif direction == "Front":
+            target_pos[1] += offset
+        elif direction == "Behind":
+            target_pos[1] -= offset
+        elif direction == "Left Front":
+            target_pos[0] -= offset
+            target_pos[1] += offset
+        elif direction == "Left Behind":
+            target_pos[0] -= offset
+            target_pos[1] -= offset
+        elif direction == "Right Front":
+            target_pos[0] += offset
+            target_pos[1] += offset
+        elif direction == "Right Behind":
+            target_pos[0] += offset
+            target_pos[1] -= offset
+        
+        # calculate the distance between all red points and the target point cloud center
+        dists = np.linalg.norm(red_points[:,:2] - target_pos[:2], axis=1)
+        dists = dists ** 0.95
+
+            # 初始化颜色矩阵（全为0，RGB）
+        heatmap_colors = np.zeros((len(points), 3))
+
+            # 对于红色点进行处理
+        valid_dists = dists.copy()
+        valid_dists[valid_dists > 150] = 150  # 将所有大于150的距离值截断为150
+
+            # 将距离映射到R通道值，距离为0时R=1，距离为150时R=0
+        r_channel_values = 1 - (valid_dists / 150)
+        # 将映射后的R通道值赋给红色点的R通道，G和B通道保持为0
+        heatmap_colors[red_mask, 0] = r_channel_values  # 只设置红色通道
+        # 对蓝色和黑色点，保持颜色为0（默认）
+        heatmap_colors[blue_mask | black_mask] = [0, 0, 0]
+        
+        '''
+        # for blue or black points, set their heatmap to 0 directly
+        invalid_mask = blue_mask[red_mask] | black_mask[red_mask]
+        
+        # calculate the heatmap value of valid points (1 when the distance is 0, 0 when the distance is greater than 10, linear distribution between 0 and 10)
+        valid_mask = ~invalid_mask
+        valid_dists = dists[valid_mask]
+
+        heatmap_values = np.zeros_like(dists)
+        heatmap_values[valid_dists <= 150] = 1 - valid_dists[valid_dists <= 150] / 150
+        heatmap_values[valid_dists == 0] = 1  # set the points with distance 0 to 1
+        heatmap[red_mask] = heatmap_values
+
+        # use Turbo colormap
+        turbo_colormap = plt.get_cmap('turbo')
+
+        # set the color of points with heatmap value 0 (blue and black points and red points with distance greater than 10) to the color corresponding to 0 in the Turbo colormap
+        heatmap_colors = np.zeros((len(points), 3))
+        heatmap_colors[red_mask] = turbo_colormap(heatmap_values)[:, :3]  # convert the heatmap value to color
+        
+        # set the color of blue and black points to the color corresponding to 0 in the Turbo colormap
+        zero_color = turbo_colormap(0)[:3]
+        heatmap_colors[blue_mask | black_mask] = zero_color
+        '''
+        # update the point cloud color
+        points_no_obj_scene.colors = o3d.utility.Vector3dVector(heatmap_colors)
+        #o3d.visualization.draw_geometries([points_no_obj_scene])
+        # add the point cloud to the list
+        pcd_without_ground = remove_ground(points_no_obj_scene)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        o3d.io.write_point_cloud(f"{output_dir}/mask_{direction}.ply", pcd_without_ground)
+        print(f"{output_dir}/mask_{direction}.ply is done")
+
+    return pcd_mask_8_direction_list
 
 def pcd_mask_preprocessing_red_label(points_scene, points_no_obj_scene=None):
     "generate red label"
@@ -353,69 +439,88 @@ if __name__ == "__main__":
     # dataset/scene_RGBD_mask/scene_name/remove_obj/ply
 
     # config
+    # 目前先从 scene_RGBD_mask_sequence 中读取数据 save到 scene_RGBD_mask_direction
+    # 结构为
+    # scene_RGBD_mask_direction
+    #   scene_name
+    #       anchor obj
+    #           mask_left.ply
+    #           mask_right.ply
+    #           mask_front.ply
+    #           mask_behind.ply
+    #           mask_left_front.ply
+    #           mask_left_behind.ply
+    #           mask_right_front.ply
+    #           mask_right_behind.ply
+    #           text_guidance.txt
     
     folder_path = "dataset/scene_RGBD_mask_sequence"
-    amount_scene = 0
-    for root,dirs,files in os.walk(folder_path):
+
+    # 遍历每个ID文件夹并查找第一个子文件夹的路径
+    for root, dirs, files in os.walk(folder_path):
         for dir_name in dirs:
-            subfolder_path = os.path.join(root, dir_name)
-            for sub_root, sub_dirs, sub_files in os.walk(subfolder_path):
-                print("amount_scene:", amount_scene)
-                for sub_dir_name in sub_dirs:
-                    sub_subfolder_path = os.path.join(sub_root, sub_dir_name)
-                    #print(sub_subfolder_path)
-                    amount_scene = amount_scene + 1
-                    #print(amount_scene)
+            id_folder_path = os.path.join(root, dir_name)
+            
+            # 获取每个ID文件夹下的所有子文件夹
+            sub_dirs = [d for d in os.listdir(id_folder_path) if os.path.isdir(os.path.join(id_folder_path, d))]
+            
+            if sub_dirs:  # 检查是否有子文件夹
+                first_sub_dir = sub_dirs[0]
+                full_path = os.path.join(id_folder_path, first_sub_dir)
+                
+                print(f"ID文件夹: {dir_name}, 第一个子文件夹路径: {full_path}")
 
-                    base = sub_subfolder_path
-                    image_path = base + "/with_obj/test_pbr/000000/rgb/000000.jpg"
-                    depth_path = base + "/with_obj/test_pbr/000000/depth_noise/000000.png"
-                    mask_hdf5_path = base + "/with_obj/0.hdf5"
-                    output_dir = base
-                    depth_removed_obj = base + "/no_obj/test_pbr/000000/depth_noise/000000.png"
-                    mask_hdf5_removed_obj = base + "/no_obj/0.hdf5"
-                    ref_image_path = base + "/RGB_ref.jpg"
-                    start_time = time.time()
-                    ply_path = os.path.join(base, "mask_red.ply")
-                    if os.path.exists(ply_path):
-                        print(f"{output_dir}/mask_red.ply already exists")
-                        continue
-                    else:
-                        RGBD2MaskPC(depth_path=depth_path, 
-                                    mask_hd5f_path=mask_hdf5_path,
-                                    output_dir=output_dir,
-                                    reprocessing_flag=True,
-                                    depth_removed_obj= depth_removed_obj,
-                                    mask_hd5f_removed_obj=mask_hdf5_removed_obj)
-                    end_time = time.time()
-                    print(f"{output_dir} is done, comsuming {end_time - start_time} s")
-               
-                break
+            # 仅处理第一个ID文件夹
+
+            # 读取第一个子文件夹中的json文件 找到anchor obj
+            json_file = os.path.join(id_folder_path, "text_guidance.json")
+            with open(json_file, 'r') as f:
+                data = json.load(f)
+                for key, value in data.items():
+                    anchor_obj = value[0] #eg: "the red cup"
+                    anchor_obj_short = value[2] #eg: "cup"
+                    break
+            
+            data_dict = {}
+            data_dict["Left"] = [anchor_obj, f"Left {anchor_obj}", anchor_obj_short, "Left"]
+            data_dict["Right"] = [anchor_obj, f"Right {anchor_obj}", anchor_obj_short, "Right"]
+            data_dict["Front"] = [anchor_obj, f"Front {anchor_obj}", anchor_obj_short, "Front"]
+            data_dict["Behind"] = [anchor_obj, f"Behind {anchor_obj}", anchor_obj_short, "Behind"]
+            data_dict["Left Front"] = [anchor_obj, f"Left Front {anchor_obj}", anchor_obj_short, "Left Front"]
+            data_dict["Left Behind"] = [anchor_obj, f"Left Behind {anchor_obj}", anchor_obj_short, "Left Behind"]
+            data_dict["Right Front"] = [anchor_obj, f"Right Front {anchor_obj}", anchor_obj_short, "Right Front"]
+            data_dict["Right Behind"] = [anchor_obj, f"Right Behind {anchor_obj}", anchor_obj_short, "Right Behind"]
+
+            
+            id_dir = dir_name
+            base = full_path
+            image_path = base + "/with_obj/test_pbr/000000/rgb/000000.jpg"
+            depth_path = base + "/with_obj/test_pbr/000000/depth_noise/000000.png"
+            mask_hdf5_path = base + "/with_obj/0.hdf5"
+            output_dir = base.replace("scene_RGBD_mask_sequence", "scene_RGBD_mask_direction")
+            depth_removed_obj = base + "/no_obj/test_pbr/000000/depth_noise/000000.png"
+            mask_hdf5_removed_obj = base + "/no_obj/0.hdf5"
+            # save the image without removed obj
+            source_image_path = base + "/no_obj/test_pbr/000000/rgb/000000.jpg"
+            target_image_path = base.replace("scene_RGBD_mask_sequence", "scene_RGBD_mask_direction") + "/img_without_removed_obj.jpg"
+            destination_dir = os.path.dirname(target_image_path)
+            if not os.path.exists(destination_dir):
+                os.makedirs(destination_dir)
+
+            shutil.copyfile(source_image_path, target_image_path)
+            print(f"copy {source_image_path} to {target_image_path}")
+
+            with open(f"{output_dir}/text_guidance.json", "w", encoding='utf-8') as f:
+                f.write(json.dumps(data_dict))
+            print(f"write {output_dir}/text_guidance.json")
+
+            RGBD2MaskPC_direction(depth_path=depth_path, 
+            mask_hd5f_path=mask_hdf5_path,
+            output_dir=output_dir,
+            reprocessing_flag=True,
+            depth_removed_obj= depth_removed_obj,
+            mask_hd5f_removed_obj=mask_hdf5_removed_obj)
+            
+        
+        # 因为我们只需要查找ID文件夹，所以在找到第一个层级的子文件夹后就可以停止递归
         break
-'''
-    base = "dataset/scene_RGBD_mask/id7/book_0001_purple" # only part need to be changed
-
-    image_path = base + "/with_obj/test_pbr/000000/rgb/000000.jpg"
-    depth_path = base + "/with_obj/test_pbr/000000/depth/000000.png"
-    mask_hdf5_path = base + "/with_obj/0.hdf5"
-    output_dir = base
-    depth_removed_obj = base + "/no_obj/test_pbr/000000/depth/000000.png"
-    mask_hdf5_removed_obj = base + "/no_obj/0.hdf5"
-    ref_image_path = base + "/RGB_ref.jpg"
-'''
-
-    
-    #center_x, center_y = rgb_obj_dect(image_path, text_prompt)
-    #print("rgb_obj_dect is done")
-    #RGBD2MaskPC(depth_path=depth_path, 
-    #            mask_hd5f_path=mask_hdf5_path,
-    #            output_dir=output_dir,
-    #            reprocessing_flag=True,
-    #            depth_removed_obj= depth_removed_obj,
-    #            mask_hd5f_removed_obj=mask_hdf5_removed_obj
-    #           )
-    #print("-----RGBD2MaskPC done-----")
-    #RGB2RefMaskPC(depth_path=depth_path,
-    #              ref_image_path=ref_image_path,
-    #              out_dir=output_dir)
-    #print("-----RGBD2RefMaskPC done-----")
---
+++ GeoL_net/dataset_gen/RGBD_mask2direction_mult.py
@@ -0,0 +1,219 @@
+"""
+    Generate the dataset for the direction estimation task from scene_RGBD
+"""
+import cv2
+import os
+import numpy as np
+from GeoL_net.dataset_gen.RGBD2PC import backproject, visualize_points
+import open3d as o3d
+import random
+import shutil
+def RGBD_mask2direction_mult(id_obj_file_dir, out_put_dir):
+    """
+    Generate the dataset for the direction estimation task from scene_RGBD
+
+    Params:
+    id_obj_file_dir: the directory of the id_obj_file, e,g. dataset/scene_RGBD_mask/id0/bottle_0001_plastic
+    out_put_dir: the output directory
+
+    Return:
+    None
+    """
+    # 进入到id_obj_file_dir 读取其中的mask_with_obj.jpg
+    img_mask_with_obj = cv2.imread(os.path.join(id_obj_file_dir, "mask_with_obj.png"))
+    color = np.array(img_mask_with_obj) / 255.0
+
+    # 读取depth.img
+    depth_path = os.path.join(id_obj_file_dir, "with_obj/test_pbr/000000/depth_noise/000000.png")
+    depth = np.array(cv2.imread(depth_path, cv2.IMREAD_UNCHANGED))
+  
+
+    # camera intrinsc matrix
+    intr = np.array([[591.0125 ,   0.     , 322.525  ],
+                     [  0.     , 590.16775, 244.11084],
+                     [  0.     ,   0.     ,   1.     ]])
+        # backprojection
+    points_scene, scene_idx = backproject(
+                depth,
+                intr,
+                np.logical_and(depth > 0, depth > 0),
+                NOCS_convention=False,
+            )
+
+        # camera rotation 
+    cam_rotation_matrix = np.array([
+        [1, 0, 0],
+        [0,0.8,-0.6],
+        [0,0.6,0.8]
+    ])
+
+    # rotate the points
+    points_scene = (cam_rotation_matrix @ points_scene.T).T
+    colors_scene = color[scene_idx[0], scene_idx[1]]
+    pcd_scene = visualize_points(points_scene, colors_scene)
+
+    #o3d.io.write_point_cloud(os.path.join(out_put_dir, "scene.ply"), pcd_scene)
+    pcd_mask_preprocessing_direction_red_label(pcd_scene, out_put_dir)
+
+
+def is_blue(color, threshold=0.9):
+    return color[0] < (1 - threshold) and color[1] < (1 - threshold) and color[2] > threshold
+
+def is_black(color, threshold=0.1):
+    return color[0] < threshold and color[1] < threshold and color[2] < threshold
+
+def is_green(color, threshold=0.9):
+    return color[0] < (1 - threshold) and color[1] > threshold and color[2] < (1 - threshold)
+
+def is_red(color, threshold=0.9):
+    return color[0] > threshold and color[1] < (1 - threshold) and color[2] < (1 - threshold)
+
+
+def compute_distance_matrix(points_a, points_b):
+    distances = np.sqrt(np.sum((points_a[:, None] - points_b[None, :])**2, axis=2))
+    return distances
+
+def pcd_mask_preprocessing_direction_red_label(points_scene, output_dir = None):
+    " generate turbo heatmap label in 8 directions"
+    " generate red label"
+
+    colors = np.asarray(points_scene.colors)
+    points = np.asarray(points_scene.points)
+
+    # get the mean of the green points in the points scene
+    green_mask = np.apply_along_axis(is_green, 1, colors)
+    green_points = points[green_mask]
+    green_pcd = o3d.geometry.PointCloud()
+    green_pcd.points = o3d.utility.Vector3dVector(green_points)
+    green_pcd_center = green_pcd.get_center()
+
+
+    # get the blue points
+    blue_mask = np.apply_along_axis(is_blue, 1, colors)
+    blue_points = points[blue_mask]
+    blue_pcd = o3d.geometry.PointCloud()
+    blue_pcd.points = o3d.utility.Vector3dVector(blue_points)
+
+    # get the black points
+    black_mask = np.apply_along_axis(is_black, 1, colors)
+    z_max = blue_pcd.get_axis_aligned_bounding_box().get_max_bound()[2]
+    z_center = blue_pcd.get_axis_aligned_bounding_box().get_center()[2]
+    z_range_mask = (points[:, 2] <= z_max + 10) & (points[:, 2] >= z_center)
+    target_mask = z_range_mask & black_mask
+
+    colors[target_mask] = [1, 0, 0]  # set the color of these points to red, representing the desktop
+    points_scene.colors = o3d.utility.Vector3dVector(colors)
+
+    # get the red points
+    red_mask = np.all(colors == [1, 0, 0], axis=1)
+    red_points = points[red_mask]
+
+    # re-extract 
+    black_mask = np.apply_along_axis(is_black, 1, colors) # groudn and table
+    blue_mask = np.apply_along_axis(is_blue, 1, colors) # obj
+    red_mask = np.all(colors == [1, 0, 0], axis=1) # tabletop
+   
+    offset = random.uniform(100, 300)
+
+    for direction in ["Left", "Right", "Front", "Behind", "Left Front", "Left Behind", "Right Front", "Right Behind"]:
+        target_pos = green_pcd_center.copy()
+        if direction == "Left":
+            target_pos[0] -= offset
+        elif direction == "Right":
+            target_pos[0] += offset
+        elif direction == "Front":
+            target_pos[1] += offset
+        elif direction == "Behind":
+            target_pos[1] -= offset
+        elif direction == "Left Front":
+            target_pos[0] -= offset
+            target_pos[1] += offset
+        elif direction == "Left Behind":
+            target_pos[0] -= offset
+            target_pos[1] -= offset
+        elif direction == "Right Front":
+            target_pos[0] += offset
+            target_pos[1] += offset
+        elif direction == "Right Behind":
+            target_pos[0] += offset
+            target_pos[1] -= offset
+        
+        # calculate the distance between all red points and the target point cloud center
+        dists = np.linalg.norm(red_points[:,:2] - target_pos[:2], axis=1)
+        dists = dists ** 0.95
+
+            # 初始化颜色矩阵（全为0，RGB）
+        heatmap_colors = np.zeros((len(points), 3))
+
+            # 对于红色点进行处理
+        valid_dists = dists.copy()
+        valid_dists[valid_dists > 150] = 150  # 将所有大于150的距离值截断为150
+
+            # 将距离映射到R通道值，距离为0时R=1，距离为150时R=0
+        r_channel_values = 1 - (valid_dists / 150)
+        # 将映射后的R通道值赋给红色点的R通道，G和B通道保持为0
+        heatmap_colors[red_mask, 0] = r_channel_values  # 只设置红色通道
+
+        # 保留绿色点的颜色
+        heatmap_colors[green_mask] = colors[green_mask]
+          
+        # update the point cloud color
+        points_scene.colors = o3d.utility.Vector3dVector(heatmap_colors)
+        #o3d.visualization.draw_geometries([points_no_obj_scene])
+        # add the point cloud to the list
+        #pcd_without_ground = remove_ground(points_no_obj_scene)
+        #if not os.path.exists(output_dir):
+        #    os.makedirs(output_dir)
+        o3d.io.write_point_cloud(f"{output_dir}/mask_{direction}.ply", points_scene)
+        print(f"{output_dir}/mask_{direction}.ply is done")
+
+
+if __name__ == "__main__":
+    id_obj_file_dir = "dataset/scene_RGBD_mask/id695_2/monitor_0011_white"
+    output_dir = id_obj_file_dir.replace("scene_RGBD_mask", "scene_RGBD_mask_direction_mult")
+    # check if the out_put_dir exists
+    # 遍历得到scene_RGBD_mask下的所有文件夹中的子文件夹 例如id695_2/monitor_0011_white
+    dir_name = "dataset/scene_RGBD_mask"
+    id_obj_dir_list = []
+    amount = 0
+    for root, dirs, files in os.walk(dir_name):
+        for dir in dirs:
+            id_obj_file_dir = os.path.join(dir_name, dir)
+            for root, dirs, files in os.walk(id_obj_file_dir):
+                sub_amout = 0 # 3 cases each id 
+                for dir in dirs:
+                    id_obj_dir = os.path.join(id_obj_file_dir, dir)
+                    #id_obj_dir_list.append(id_obj_dir)
+                    output_dir = id_obj_dir.replace("scene_RGBD_mask", "scene_RGBD_mask_direction_mult")
+                    if not os.path.exists(output_dir):
+                        os.makedirs(output_dir)
+                    RGBD_mask2direction_mult(id_obj_dir, output_dir)
+                    img_path = os.path.join(id_obj_dir, "with_obj/test_pbr/000000/rgb/000000.jpg")
+                    target_path = os.path.join(output_dir, "img.jpg")
+                    shutil.copy(img_path, target_path)
+                    amount += 1
+                    print(f"{amount} is done")
+                    #print(id_obj_dir_list)
+                    sub_amout += 1
+                    print(sub_amout)
+                    if sub_amout == 3: # 3 cases each id 
+                        break
+                break
+        break
+
+    """
+    while amount < 200:
+        id_obj_dir = random.choice(id_obj_dir_list)
+        output_dir = id_obj_dir.replace("scene_RGBD_mask", "scene_RGBD_mask_direction_mult")
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        RGBD_mask2direction_mult(id_obj_dir, output_dir)
+        img_path = os.path.join(id_obj_dir, "with_obj/test_pbr/000000/rgb/000000.jpg")
+        target_path = os.path.join(output_dir, "img.jpg")
+        shutil.copy(img_path, target_path)
+        amount += 1
+        print(f"{amount} is done")
+    """
+    #if not os.path.exists(output_dir):
+    #    os.makedirs(output_dir)
+    #RGBD_mask2direction_mult(id_obj_file_dir, output_dir)
